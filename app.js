6e9a3ca378a1885c20ce19fec8a75b3f1e323413
Duc Vu <viet.vu001@stud.fh-dortmund.de>
2019-03-30 10:16:00 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const mongoose = require('mongoose');
+const mongoDbUser = "mongodb://localhost:27017/albumDB";
+mongoose.connect(mongoDbUser, {useNewUrlParser: true});
+
+// Create Schema
+const postSchema = new mongoose.Schema({
+    title: String,
+    avatar: String,
+    content: String,
+});
+
+// Create Model
+const postModel = mongoose.model('post', postSchema);
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.render('landing');
+});
+
+app.get('/home', (req, res) => {
+    postModel.find({}, (err, results) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('home', {posts: results});
+        }
+    });
+
+});
+
+app.get('/compose', (req, res) => {
+    res.render('compose');
+});
+
+app.post('/compose', (req, res) => {
+    const newPost = new postModel({
+        title: req.body.title,
+        avatar: req.body.avatar,
+        content: req.body.postBody
+    });
+    newPost.save();
+
+    res.redirect('/home');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.listen(3030, () => {
+    console.log('Server is running on port 3030')
+});
